3dc26b7a7a0fc09266075e6c772a5043b0a477da
Tolu Oluwagbemi <tulburg@yahoo.com>
2023-02-21 00:48:03 +0100

    Update to instance approach

--- express/index.ts
+++ express/index.ts
@@ -34,19 +34,21 @@ export class Fern {
   options: FernConfiguration;
   private app: any;
   private server: any;
-  response?: core.Response;
-  request?: core.Request;
-  callee: [CalleeFunction];
-  registry: { [key: string]: [CalleeFunction] };
-
-  nextDb: any[] = [] as any;
-  nextBody: { [key: string]: any } = {} as any;
-  nextStore: { [key: string]: any } = {} as any;
-  nextHeader: { [key: string]: any } = {} as any;
-  nextParams: { [key: string]: any } = {} as any;
-  nextQuery: { [key: string]: any } = {} as any;
-  nextMethod: 'post' | 'get' | 'delete' = 'get';
-
+  registry: {
+    [key: string]: {
+      callee: [CalleeFunction],
+      request: core.Request,
+      response: core.Response,
+      nextDb: any[],
+      nextBody: { [key: string]: any },
+      nextStore: { [key: string]: any },
+      nextHeader: { [key: string]: any },
+      nextParams: { [key: string]: any },
+      nextQuery: { [key: string]: any },
+      nextMethod: 'post' | 'get' | 'delete'
+    }
+  }
+  key: string = "";
   defaultOptions = {
     useJSON: true,
     driver: 'express',
@@ -60,7 +62,8 @@ export class Fern {
       this.app = express.default();
       this.use = this.app.use.bind(this.app);
       this.app.response.sendOk = function (json: { [key: string]: any }) {
-        log(chalk.green('Send => '), 200, json);
+        const logData = json.data;
+        log(chalk.green('Send => '), 200, { ...json, data: logData?.toString() });
         json.status = 200;
         this.status(200).json(json);
       };
@@ -81,28 +84,25 @@ export class Fern {
       this.server.listen(options.port || 8000);
       log(`>>  listening at ${options.port || 8000}`);
     }
-    this.callee = [] as any;
     this.registry = {} as any;
   }
 
   endpoint(path: string, method: 'POST' | 'GET' | 'DELETE'): Fern {
     method = method.toLowerCase() as any;
-
-    this.registry[method + ':' + path] = [] as any
-    this.callee = this.registry[method + ':' + path];
+    this.key = method + ':' + path;
+    this.registry[this.key] = { callee: [], options: this.options } as any
     this.app[method](path, (req: any, res: any) => {
       log(chalk.magenta('Receive => ') + method + ':' + path);
-      this.request = req;
-      this.response = res;
+      const key = method + ':' + path;
+      const instance = this.registry[key]
+      instance.request = req;
+      instance.response = res;
+      instance.nextMethod = method.toUpperCase() as any;
+      instance.nextStore = {}
+
       let index = 0;
-      this.nextBody = undefined as any;
-      this.nextMethod = method.toUpperCase() as any;
-      this.nextParams = undefined as any;
-      this.nextQuery = undefined as any;
-      this.nextDb = undefined as any;
-      this.nextStore = {} as any;
-      this.nextHeader = undefined as any;
-      const callee = this.registry[method + ':' + path];
+      const callee = this.registry[method + ':' + path].callee;
+      let response = res;
       const callNext = () => {
         if (index < callee.length) {
           const fn = callee[index];
@@ -113,9 +113,9 @@ export class Fern {
                 index++;
                 callNext();
               } else {
-                if (!v) this.response?.sendError({ code: 500, message: 'FernError: Function failed' });
-                else if (v.code !== 200) this.response?.sendError(v);
-                else this.response?.sendOk(v);
+                if (!v) response?.sendError({ code: 500, message: 'FernError: Function failed' });
+                else if (v.code !== 200) response?.sendError(v);
+                else response?.sendOk(v);
               }
             })
           } else if (res === true) {
@@ -123,225 +123,184 @@ export class Fern {
             callNext();
           } else {
             const result = res as { code: number, message?: string, stack?: any };
-            if (!res) this.response?.sendError({ code: 500, message: 'FernError: Function failed' });
-            else if (result.code !== 200) this.response?.sendError(result);
-            else this.response?.sendOk(result);
+            if (!res) response?.sendError({ code: 500, message: 'FernError: Function failed' });
+            else if (result.code !== 200) response?.sendError(result);
+            else response?.sendOk(result);
           };
         }
       }
       callNext();
     });
-    this.callee.push((fern: Fern) => {
-      fern.nextMethod = method as any;
-      return true;
-    });
     return this;
   }
 
   mapBody(keys: string[], success?: CalleeFunction, fail?: CalleeFunction) {
+    const instance = this.registry[this.key]
     const fn: CalleeFunction = () => {
-      if (!this.request?.body) return {
+      if (!instance.request.body) return {
         code: 400, message: 'Invalid request'
       }
       let checks = 0;
       keys.forEach(k => {
-        if (this.request?.body.hasOwnProperty(k)) {
-          this.nextBody = this.nextBody || {} as any;
-          this.nextBody[k] = this.request?.body[k];
+        if (instance.request?.body.hasOwnProperty(k)) {
+          instance.nextBody = instance.nextBody || {} as any;
+          instance.nextBody[k] = instance.request?.body[k];
           checks++;
         }
       });
       if (checks === keys.length) return success ? success(true) : true;
       return fail ? fail({ code: 403, message: 'Bad Request' }) : { code: 403, message: 'Bad Request' };
     }
-    this.callee.push(fn);
+    instance.callee.push(fn);
     return this;
   }
 
   useBody(callback?: CalleeFunction) {
+    const instance = this.registry[this.key]
     const fn = () => {
-      if (callback) return callback(this.nextBody, this);
+      if (callback) return callback(instance.nextBody, instance);
       else return true;
     }
-    this.callee.push(fn as CalleeFunction);
+    instance.callee.push(fn as CalleeFunction);
     return this;
   }
 
   mapParams(keys: string[], success?: CalleeFunction, fail?: CalleeFunction) {
+    const instance = this.registry[this.key]
     const fn: CalleeFunction = () => {
-      if (!this.request?.params) return {
+      if (!instance.request?.params) return {
         code: 400, message: 'Invalid request'
       }
       let checks = 0;
       keys.forEach(k => {
-        if (this.request?.params.hasOwnProperty(k)) {
-          this.nextParams = this.nextParams || {} as any;
-          this.nextParams[k] = this.request?.params[k];
+        if (instance.request?.params.hasOwnProperty(k)) {
+          instance.nextParams = instance.nextParams || {} as any;
+          instance.nextParams[k] = instance.request?.params[k];
           checks++;
         }
       });
       if (checks === keys.length) return success ? success(true) : true;
       return fail ? fail({ code: 403, message: 'Bad Request' }) : { code: 403, message: 'Bad Request' };
     }
-    this.callee.push(fn);
+    instance.callee.push(fn);
     return this;
   }
 
   useParams(callback?: CalleeFunction) {
+    const instance = this.registry[this.key]
     const fn = () => {
-      if (callback) return callback(this.nextParams, this);
+      if (callback) return callback(instance.nextParams, instance);
       else return true;
     }
-    this.callee.push(fn as CalleeFunction);
+    instance.callee.push(fn as CalleeFunction);
     return this;
   }
 
   mapQuery(keys: string[], success?: CalleeFunction, fail?: CalleeFunction) {
+    const instance = this.registry[this.key]
     const fn: CalleeFunction = () => {
-      if (!this.request?.query) return {
+      if (!instance.request?.query) return {
         code: 400, message: 'Invalid request'
       }
       let checks = 0;
       keys.forEach(k => {
-        if (this.request?.query.hasOwnProperty(k)) {
-          this.nextQuery = this.nextQuery || {} as any;
-          this.nextQuery[k] = this.request?.query[k];
+        if (instance.request?.query.hasOwnProperty(k)) {
+          instance.nextQuery = instance.nextQuery || {} as any;
+          instance.nextQuery[k] = instance.request?.query[k];
           checks++;
         }
       });
       if (checks === keys.length) return success ? success(true) : true;
       return fail ? fail({ code: 403, message: 'Bad Request' }) : { code: 403, message: 'Bad Request' };
     }
-    this.callee.push(fn);
+    instance.callee.push(fn);
     return this;
   }
 
   useQuery(callback?: CalleeFunction) {
+    const instance = this.registry[this.key]
     const fn = () => {
-      if (callback) return callback(this.nextQuery, this);
+      if (callback) return callback(instance.nextQuery, instance);
       else return true;
     }
-    this.callee.push(fn as CalleeFunction);
+    instance.callee.push(fn as CalleeFunction);
     return this;
   }
 
   mapHeader(keys: string[], success?: CalleeFunction, fail?: CalleeFunction) {
+    const instance = this.registry[this.key]
     const fn: CalleeFunction = () => {
-      if (!this.request?.headers) return {
+      if (!instance.request?.headers) return {
         code: 400, message: 'Invalid request'
       }
       let checks = 0;
       keys.forEach(k => {
-        if (this.request?.headers.hasOwnProperty(k)) {
-          this.nextHeader = this.nextHeader || {} as any;
-          this.nextHeader[k] = this.request?.headers[k];
+        if (instance.request?.headers.hasOwnProperty(k)) {
+          instance.nextHeader = instance.nextHeader || {} as any;
+          instance.nextHeader[k] = instance.request?.headers[k];
           checks++;
         }
       });
       if (checks === keys.length) return success ? success(true) : true;
       return fail ? fail({ code: 403, message: 'Bad Request' }) : { code: 403, message: 'Bad Request' };
     }
-    this.callee.push(fn);
+    instance.callee.push(fn);
     return this;
   }
 
   useHeader(callback?: CalleeFunction) {
+    const instance = this.registry[this.key]
     const fn = () => {
-      if (callback) return callback(this.nextHeader, this);
+      if (callback) return callback(instance.nextHeader, instance);
       else return true;
     }
-    this.callee.push(fn as CalleeFunction);
+    instance.callee.push(fn as CalleeFunction);
     return this;
   }
-
-  // mapHeader
-  // useHeader
-  // useStore
 
   mapDB(...args: any[]) {
+    const instance = this.registry[this.key]
     const fn: CalleeFunction = () => {
-      this.nextDb = args;
+      instance.nextDb = args;
       return true;
     }
-    this.callee.push(fn);
+    instance.callee.push(fn);
     return this;
   }
 
-  useDB(pFn: (db: any[], fern: Fern) => Promise<boolean>) {
+  useDB(pFn: (db: any[], fern: any) => Promise<boolean>) {
+    const instance = this.registry[this.key]
     const fn: CalleeFunction = async () => {
       let res = false;
       try {
-        res = await pFn(this.nextDb, this);
+        res = await pFn(instance.nextDb, instance);
       } catch (e) { log(e) };
       return res;
     }
-    this.callee.push(fn);
+    instance.callee.push(fn);
     return this;
   }
 
-  // useAuthentication() {
-  //   const fn: CalleeFunction = () => {
-  //     const headerToken = this.request?.headers.authorization;
-  //     if(headerToken && headerToken.slice(0, 6) === 'Bearer') {
-  //       let token = headerToken.replace('Bearer ', '');
-  //       if(token.trim().length === 0) {
-  //         return { code: 401, message: 'Authorization failed' };
-  //       }
-  //       jwt.verify(token, process.env.jwtKey, function(err: any, data: TokenData) {
-  //         if(err || data.iss !== 'x-plane.app.server') {
-  //           return { code: 401, message: 'Unauthorized' };
-  //         }else if(new Date(data.expiry) < new Date()) {
-  //           return { code: 400, message: 'Invalid Token' };
-  //         }else return true;
-  //       });
-  //     }else {
-  //       return { code: 403, message: 'Authorization failed' }
-  //     }
-  //     return false;
-  //   }
-  //   
-  //   this.callee.push(fn);
-  //   return this;
-  // }
-
   useStore(callback?: CalleeFunction) {
+    const instance = this.registry[this.key]
     const fn = () => {
-      if (callback) return callback(this.nextStore, this);
+      if (callback) return callback(instance.nextStore, instance);
       else return true;
     }
-    this.callee.push(fn as CalleeFunction);
+    instance.callee.push(fn as CalleeFunction);
     return this;
   }
 
   send(data: string | { message: string } | any | ((fern: Fern) => string | { message: string } | any)) {
+    const instance = this.registry[this.key]
     const fn: CalleeFunction = () => {
-      if (typeof data === 'function') this.response?.sendOk(data(this))
-      else this.response?.sendOk(data);
+      if (typeof data === 'function') instance.response?.sendOk(data(instance))
+      else instance.response?.sendOk(data);
       return true;
     };
-    this.callee.push(fn);
+    instance.callee.push(fn);
   }
 
-  //
-  // useDB(callable: (map: string[] | [string[]], success: any, fail: any) => any, map: string[] | [string[]], success: any, fail: any) {
-  //   callable(map, success, fail);
-  // }
-  //
-  // mapInputs(callable: Function, field: [string]) {
-  //
-  // }
-
-
 }
-
-// Other Database functions for useDB =>
-// CheckIfExists
-// Create
-// CreateOrUpdate
-// Delete
-// Fetch
-// FetchWhere
-//
-
 
 export default Fern;
