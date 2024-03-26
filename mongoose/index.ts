526114320c70dbfb2c26d2f14d3acae35998b70e
Tolu Oluwagbemi <tulburg@yahoo.com>
2024-03-27 00:59:32 +0100

    Add long await upgrades

--- mongoose/index.ts
+++ mongoose/index.ts
@@ -1,13 +1,18 @@
 type Weave = {
-  body: { [key: string]: any }
-  header: { [key: string]: any }
-  params: { [key: string]: any }
-  store: { [key: string]: any }
-  query: { [key: string]: any }
-  options: { dbConnection: any }
-  db: any[]
+  body: { [key: string]: any };
+  header: { [key: string]: any };
+  params: { [key: string]: any };
+  store: { [key: string]: any };
+  query: { [key: string]: any };
+  options: { dbConnection: any };
+  db: any[];
 };
-type CalleeFunction<T = any> = (...args: T[]) => boolean | Promise<boolean | { code: number, message?: string, stack?: any }> | { code: number, message?: string, stack?: any }
+type CalleeFunction<T = any> = (
+  ...args: T[]
+) =>
+  | boolean
+  | Promise<boolean | { code: number; message?: string; stack?: any }>
+  | { code: number; message?: string; stack?: any };
 
 const Call = (fn: any, ...args: any[]) => {
   try {
@@ -15,137 +20,235 @@ const Call = (fn: any, ...args: any[]) => {
   } catch (e) {
     return e;
   }
-}
+};
 const part = {
-  fromBody: function (args: string[], exists?: CalleeFunction, doesNotExist?: CalleeFunction) {
-    return this._from('body', args, exists, doesNotExist);
+  fromBody: function (
+    args: string[],
+    exists?: CalleeFunction,
+    doesNotExist?: CalleeFunction
+  ) {
+    return this._from("body", args, exists, doesNotExist);
   },
 
-  fromQuery: function (args: string[], exists?: CalleeFunction, doesNotExist?: CalleeFunction) {
-    return this._from('query', args, exists, doesNotExist);
+  fromQuery: function (
+    args: string[],
+    exists?: CalleeFunction,
+    doesNotExist?: CalleeFunction
+  ) {
+    return this._from("query", args, exists, doesNotExist);
   },
 
-  fromHeader: function (args: string[], exists?: CalleeFunction, doesNotExist?: CalleeFunction) {
-    return this._from('header', args, exists, doesNotExist);
+  fromHeader: function (
+    args: string[],
+    exists?: CalleeFunction,
+    doesNotExist?: CalleeFunction
+  ) {
+    return this._from("header", args, exists, doesNotExist);
   },
 
-  fromParams: function (args: string[], exists?: CalleeFunction, doesNotExist?: CalleeFunction) {
-    return this._from('params', args, exists, doesNotExist);
+  fromParams: function (
+    args: string[],
+    exists?: CalleeFunction,
+    doesNotExist?: CalleeFunction
+  ) {
+    return this._from("params", args, exists, doesNotExist);
   },
 
-  fromStore: function (args: string[], exists?: CalleeFunction, doesNotExist?: CalleeFunction) {
-    return this._from('store', args, exists, doesNotExist);
+  fromStore: function (
+    args: string[],
+    exists?: CalleeFunction,
+    doesNotExist?: CalleeFunction
+  ) {
+    return this._from("store", args, exists, doesNotExist);
   },
 
-  _from: function (type: 'body' | 'params' | 'header' | 'store' | 'query', args: string[], exists?: CalleeFunction, doesNotExist?: CalleeFunction) {
+  _from: function (
+    type: "body" | "params" | "header" | "store" | "query",
+    args: string[],
+    exists?: CalleeFunction,
+    doesNotExist?: CalleeFunction
+  ) {
     return (_: any[], instance: Weave) => {
       const source = {
         body: instance.body,
         params: instance.params,
         header: instance.header,
         store: instance.store,
-        query: instance.query
+        query: instance.query,
       };
       if (!source[type]) throw "Source (" + type + ") is not mapped";
       else {
         const body: any = {};
-        args.forEach(k => {
+        args.forEach((k) => {
           body[k] = source[type][k];
         });
-        if (this.limit) {
-          return this._execute(instance, this.limit, body, exists, doesNotExist);
-        } else return this._execute(instance, body, exists, doesNotExist);
+        try {
+          if (this.limit) {
+            return this._execute(
+              instance,
+              this.limit,
+              body,
+              exists,
+              doesNotExist
+            );
+          } else return this._execute(instance, body, exists, doesNotExist);
+        } catch (e) {
+          console.log(e);
+        }
       }
-    }
-  }
-}
-
-export const CheckIfExists = Object.assign({
-  _execute: async function (instance: Weave, params: { [key: string]: any }, exists?: CalleeFunction, doesNotExist?: CalleeFunction) {
-    if (instance.db.length === 0) return false;
-    const { dbConnection } = instance.options;
-    const { db } = instance;
-    const model = dbConnection?.model(db[0], db[1]);
-    const check = await model?.findOne(params);
-    if (check) {
-      if (exists) return exists(instance);
-      else return true;
-    } else {
-      if (doesNotExist) return doesNotExist(instance);
-      else return false;
-    }
-  }
-}, part);
-
-export const FetchWhere = Object.assign({
-  limit: function ({ start = 0, limit = 10 }: { start: number, limit: number }) {
-    this.limit = { start, limit };
-    return this;
+    };
   },
-  rawParams: function (params: any, success?: CalleeFunction, fail?: CalleeFunction) {
-    return (instance: Weave) => {
-      this._execute(instance, this.limit, params, success, fail)
-    }
-  },
-  _execute: async function (instance: Weave, limit: { start: number, limit: number }, params: { [key: string]: any }, exists?: CalleeFunction, doesNotExist?: CalleeFunction) {
-    const { dbConnection } = instance.options;
-    const { db } = instance;
-    const model = dbConnection?.model(db[0], db[1]);
-    const get = await model?.find(params, null, { skip: limit.start, limit: limit.limit });
-    if (get && get.length > 0) {
-      if (exists) return exists(get, instance);
-      else return true;
-    } else {
-      if (doesNotExist) return doesNotExist(instance);
-      else return false;
-    }
-  }
-}, part);
+};
 
-export const Count = Object.assign({
-  rawParams: function (params: any, success?: CalleeFunction, fail?: CalleeFunction) {
-    return (instance: Weave) => {
-      this._execute(instance, params, success, fail)
-    }
+export const CheckIfExists = Object.assign(
+  {
+    _execute: async function (
+      instance: Weave,
+      params: { [key: string]: any },
+      exists?: CalleeFunction,
+      doesNotExist?: CalleeFunction
+    ) {
+      if (instance.db.length === 0) return false;
+      const { dbConnection } = instance.options;
+      const { db } = instance;
+      const model = dbConnection?.model(db[0], db[1]);
+      const check = await model?.findOne(params);
+      if (check) {
+        if (exists) return exists(instance);
+        else return true;
+      } else {
+        if (doesNotExist) return doesNotExist(instance);
+        else return false;
+      }
+    },
   },
-  _execute: async function (instance: Weave, params: { [key: string]: any }, exists?: CalleeFunction, doesNotExist?: CalleeFunction) {
-    const { dbConnection } = instance.options;
-    const { db } = instance;
-    const model = dbConnection?.model(db[0], db[1]);
-    const get = await model?.count(params);
-    if (get) {
-      if (exists) return exists(get, instance);
-      else return true;
-    } else {
-      if (doesNotExist) return doesNotExist(instance);
-      else return false;
-    }
-  }
-}, part);
+  part
+);
 
-export const FetchOne = Object.assign({
-  _execute: async function (instance: Weave, params: { [key: string]: any }, exists?: CalleeFunction, doesNotExist?: CalleeFunction) {
-    const { dbConnection } = instance.options;
-    const { db } = instance;
-    const model = dbConnection?.model(db[0], db[1]);
-    const get = await model?.findOne(params);
-    if (get) {
-      if (exists) return Call(exists, get, instance);
-      else return true;
-    } else {
-      if (doesNotExist) return Call(doesNotExist, instance);
-      else return false;
-    }
-  }
-}, part);
+export const FetchWhere = Object.assign(
+  {
+    limit: function ({
+      start = 0,
+      limit = 10,
+    }: {
+      start: number;
+      limit: number;
+    }) {
+      this.limit = { start, limit };
+      return this;
+    },
+    rawParams: function (
+      params: any,
+      success?: CalleeFunction,
+      fail?: CalleeFunction
+    ) {
+      return (instance: Weave) => {
+        this._execute(instance, this.limit, params, success, fail);
+      };
+    },
+    _execute: async function (
+      instance: Weave,
+      limit: { start: number; limit: number },
+      params: { [key: string]: any },
+      exists?: CalleeFunction,
+      doesNotExist?: CalleeFunction
+    ) {
+      const { dbConnection } = instance.options;
+      const { db } = instance;
+      const model = dbConnection?.model(db[0], db[1]);
+      const get = await model?.find(params, null, {
+        skip: limit.start,
+        limit: limit.limit,
+      });
+      if (get && get.length > 0) {
+        if (exists) return exists(get, instance);
+        else return true;
+      } else {
+        if (doesNotExist) return doesNotExist(instance);
+        else return false;
+      }
+    },
+  },
+  part
+);
+
+export const Count = Object.assign(
+  {
+    rawParams: function (
+      params: any,
+      success?: CalleeFunction,
+      fail?: CalleeFunction
+    ) {
+      return (instance: Weave) => {
+        this._execute(instance, params, success, fail);
+      };
+    },
+    _execute: async function (
+      instance: Weave,
+      params: { [key: string]: any },
+      exists?: CalleeFunction,
+      doesNotExist?: CalleeFunction
+    ) {
+      const { dbConnection } = instance.options;
+      const { db } = instance;
+      const model = dbConnection?.model(db[0], db[1]);
+      const get = await model?.count(params);
+      if (get) {
+        if (exists) return exists(get, instance);
+        else return true;
+      } else {
+        if (doesNotExist) return doesNotExist(instance);
+        else return false;
+      }
+    },
+  },
+  part
+);
+
+export const FetchOne = Object.assign(
+  {
+    _execute: async function (
+      instance: Weave,
+      params: { [key: string]: any },
+      exists?: CalleeFunction,
+      doesNotExist?: CalleeFunction
+    ) {
+      const { dbConnection } = instance.options;
+      const { db } = instance;
+      try {
+        const model = dbConnection?.model(db[0], db[1]);
+        const get = await model?.findOne(params);
+        if (get) {
+          if (exists) return Call(exists, get, instance);
+          else return true;
+        } else {
+          if (doesNotExist) return Call(doesNotExist, instance);
+          else return false;
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+  part
+);
 
 export const Fetch = {
-  withLimit: function (limit?: { start: number, limit: number }, success?: CalleeFunction, fail?: CalleeFunction) {
+  withLimit: function (
+    limit?: { start: number; limit: number },
+    success?: CalleeFunction,
+    fail?: CalleeFunction
+  ) {
     return (_: any, instance: Weave) => {
       return this._execute(instance, limit, success, fail);
-    }
+    };
   },
-  _execute: async function (instance: Weave, limit: { start: number, limit: number }, success?: CalleeFunction, fail?: CalleeFunction) {
+  _execute: async function (
+    instance: Weave,
+    limit: { start: number; limit: number },
+    success?: CalleeFunction,
+    fail?: CalleeFunction
+  ) {
     const { dbConnection } = instance.options;
     const { db } = instance;
     const model = dbConnection?.model(db[0], db[1]);
@@ -155,36 +258,57 @@ export const Fetch = {
     if (get) {
       return success ? success(get, instance) : true;
     } else return fail ? fail(false, instance) : false;
-  }
-}
+  },
+};
 
-export const Insert = Object.assign({
-  _execute: async function (instance: Weave, params: { [key: string]: any }, success?: CalleeFunction, fail?: CalleeFunction) {
-    const { dbConnection } = instance.options;
-    const { db } = instance;
-    const model: any = dbConnection?.model(db[0], db[1]);
-    const create = await model.create(params);
-    if (create) {
-      return success ? success(create, instance) : true;
-    } else return fail ? fail(false, instance) : false;
-  }
-}, part);
+export const Insert = Object.assign(
+  {
+    _execute: async function (
+      instance: Weave,
+      params: { [key: string]: any },
+      success?: CalleeFunction,
+      fail?: CalleeFunction
+    ) {
+      const { dbConnection } = instance.options;
+      const { db } = instance;
+      const model: any = dbConnection?.model(db[0], db[1]);
+      const create = await model.create(params);
+      if (create) {
+        return success ? success(create, instance) : true;
+      } else return fail ? fail(false, instance) : false;
+    },
+  },
+  part
+);
 
-export const InsertMany = Object.assign({
-  _execute: async function (instance: Weave, params: { [key: string]: any }, success?: CalleeFunction, fail?: CalleeFunction) {
-    const { dbConnection } = instance.options;
-    const { db } = instance;
-    const model: any = dbConnection?.model(db[0], db[1]);
-    const create = await model.insertMany(Object.values(params).flat());
-    if (create) {
-      return success ? success(create, instance) : true;
-    } else return fail ? fail(false, instance) : false;
-  }
-}, part);
+export const InsertMany = Object.assign(
+  {
+    _execute: async function (
+      instance: Weave,
+      params: { [key: string]: any },
+      success?: CalleeFunction,
+      fail?: CalleeFunction
+    ) {
+      const { dbConnection } = instance.options;
+      const { db } = instance;
+      const model: any = dbConnection?.model(db[0], db[1]);
+      const create = await model.insertMany(Object.values(params).flat());
+      if (create) {
+        return success ? success(create, instance) : true;
+      } else return fail ? fail(false, instance) : false;
+    },
+  },
+  part
+);
 
 export const InsertOrUpdateWhere = (clause: string[]) => {
   const main: any = {
-    _execute: async function (instance: Weave, params: { [key: string]: any }, success?: CalleeFunction, fail?: CalleeFunction) {
+    _execute: async function (
+      instance: Weave,
+      params: { [key: string]: any },
+      success?: CalleeFunction,
+      fail?: CalleeFunction
+    ) {
       const { dbConnection } = instance.options;
       const { db } = instance;
       const clause: any = {};
@@ -199,16 +323,20 @@ export const InsertOrUpdateWhere = (clause: string[]) => {
       if (update) {
         return success ? success(update, instance) : true;
       } else return fail ? fail(false, instance) : false;
-    }
-  }
+    },
+  };
   main.clause = clause;
-  return Object.assign(main, part)
+  return Object.assign(main, part);
 };
-
 
 export const UpdateWhere = (clause: string[]) => {
   const main: any = {
-    _execute: async function (instance: Weave, params: { [key: string]: any }, success?: CalleeFunction, fail?: CalleeFunction) {
+    _execute: async function (
+      instance: Weave,
+      params: { [key: string]: any },
+      success?: CalleeFunction,
+      fail?: CalleeFunction
+    ) {
       const { dbConnection } = instance.options;
       const { db } = instance;
       const clause: any = {};
@@ -223,24 +351,32 @@ export const UpdateWhere = (clause: string[]) => {
       if (update) {
         return success ? success(update, instance) : true;
       } else return fail ? fail(false, instance) : false;
-    }
+    },
   };
   main.clause = clause;
-  return Object.assign(main, part)
+  return Object.assign(main, part);
 };
 
-export const DeleteOne = Object.assign({
-  _execute: async function (instance: Weave, params: { [key: string]: any }, exists?: CalleeFunction, doesNotExist?: CalleeFunction) {
-    const { dbConnection } = instance.options;
-    const { db } = instance;
-    const model = dbConnection?.model(db[0], db[1]);
-    const get = await model?.deleteOne(params);
-    if (get) {
-      if (exists) return exists(get, instance);
-      else return true;
-    } else {
-      if (doesNotExist) return doesNotExist(instance);
-      else return false;
-    }
-  }
-}, part);
+export const DeleteOne = Object.assign(
+  {
+    _execute: async function (
+      instance: Weave,
+      params: { [key: string]: any },
+      exists?: CalleeFunction,
+      doesNotExist?: CalleeFunction
+    ) {
+      const { dbConnection } = instance.options;
+      const { db } = instance;
+      const model = dbConnection?.model(db[0], db[1]);
+      const get = await model?.deleteOne(params);
+      if (get) {
+        if (exists) return exists(get, instance);
+        else return true;
+      } else {
+        if (doesNotExist) return doesNotExist(instance);
+        else return false;
+      }
+    },
+  },
+  part
+);
--- socket/index.ts
+++ socket/index.ts
@@ -1,57 +1,66 @@
-import { Server, Socket as SocketIO } from 'socket.io'
+import { Server, Socket as SocketIO } from "socket.io";
 
 interface SocketOptions {
   io: Server;
   options: { [key: string]: any } & { dbConnection: any };
 }
-export type CalleeFunction<T = any> = (...args: T[]) => boolean | Promise<boolean | { code: number, message?: string, stack?: any }> | { code: number, message?: string, stack?: any }
-export const type = (o: any) => Object.prototype.toString.call(o).substr(8).replace(']', '').toLowerCase();
+export type CalleeFunction<T = any> = (
+  ...args: T[]
+) =>
+  | boolean
+  | Promise<boolean | { code: number; message?: string; stack?: any }>
+  | { code: number; message?: string; stack?: any };
+export const type = (o: any) =>
+  Object.prototype.toString.call(o).substr(8).replace("]", "").toLowerCase();
 interface SocketInstance {
-  headers: { [key: string]: any },
-  store: { [key: string]: any },
-  header: { [key: string]: any },
-  event: string,
-  db: any[]
-  data: { [key: string]: any },
-  body: { [key: string]: any },
-  privatebody: { [key: string]: any },
-  privateheaders: { [key: string]: any },
-  socket: SocketIO
-  options: SocketOptions,
-  callee: CalleeFunction[]
+  headers: { [key: string]: any };
+  store: { [key: string]: any };
+  header: { [key: string]: any };
+  event: string;
+  db: any[];
+  data: { [key: string]: any };
+  body: { [key: string]: any };
+  privatebody: { [key: string]: any };
+  privateheaders: { [key: string]: any };
+  socket: SocketIO;
+  options: SocketOptions;
+  callee: CalleeFunction[];
 }
 
 export default class Socket {
   io: Server;
-  registry: { [key: string]: any } = {} as any
-  callee: CalleeFunction[] = []
+  registry: { [key: string]: any } = {} as any;
+  callee: CalleeFunction[] = [];
   event: string = "";
   // eslint-disable-next-line no-unused-vars
   constructor(options: SocketOptions) {
     this.io = options.io;
-    this.io.on('connection', socket => {
-      Object.keys(this.registry).forEach(event => {
-        socket.on(event, data => {
+    this.io.on("connection", (socket) => {
+      Object.keys(this.registry).forEach((event) => {
+        socket.on(event, (data) => {
           const instance: SocketInstance = {
-            io: options.io, socket, event, store: {}
+            io: options.io,
+            socket,
+            event,
+            store: {},
           } as any;
-          console.log('>> ', 'Connected successfully');
+          console.log(">> ", "Connected successfully");
           instance.headers = instance.socket.handshake.headers;
           instance.privateheaders = instance.socket.handshake.headers;
           instance.options = options;
 
           instance.callee = this.registry[event];
-          console.log('<< Received ' + event + ' => ', data);
+          console.log("<< Received " + event + " => ", data);
 
           const emitError = (data: any) => {
-            console.error('Sent Error => ', data);
-            instance.socket.emit('error', data);
+            console.error("Sent Error => ", data);
+            instance.socket.emit("error", data);
           };
 
           const emit = (data: any) => {
-            console.log('>> Sent (' + instance.event + ') => ', data);
+            console.log(">> Sent (" + instance.event + ") => ", data);
             instance.socket.emit(instance.event, data);
-          }
+          };
 
           let index = 0;
           instance.data = instance.privatebody = data;
@@ -60,14 +69,17 @@ export default class Socket {
               const fn = instance.callee[index];
               try {
                 const res = fn(data, instance);
-                if (type(res) === 'promise') {
+                if (type(res) === "promise") {
                   (res as Promise<boolean>).then((v) => {
                     if (v === true) {
                       index++;
                       callNext();
                     } else {
                       if (!v)
-                        emitError({ code: 500, message: 'SocketError: Function failed' });
+                        emitError({
+                          code: 500,
+                          message: "SocketError: Function failed",
+                        });
                       else {
                         if ((v as any).code === 200) emit(v);
                         else emitError(v);
@@ -77,19 +89,36 @@ export default class Socket {
                 } else if (res === true) {
                   index++;
                   callNext();
-                } else if (type(res) === 'error') {
-                  emitError({ code: 500, message: 'SocketError: ' + ((res as any).message ? (res as any).message : 'Function failed'), stack: res })
+                } else if (type(res) === "error") {
+                  emitError({
+                    code: 500,
+                    message:
+                      "SocketError: " +
+                      ((res as any).message
+                        ? (res as any).message
+                        : "Function failed"),
+                    stack: res,
+                  });
                 } else {
                   const result = res;
                   if (!res)
-                    emitError({ code: 500, message: 'SocketError: Function failed' });
+                    emitError({
+                      code: 500,
+                      message: "SocketError: Function failed",
+                    });
                   else {
                     if ((result as any).code === 200) emit(result);
                     else emitError(result);
                   }
                 }
               } catch (e: any) {
-                emitError({ code: 500, message: 'SocketError: ' + (e.message ? e.message : ' Function failed'), stack: e })
+                emitError({
+                  code: 500,
+                  message:
+                    "SocketError: " +
+                    (e.message ? e.message : " Function failed"),
+                  stack: e,
+                });
               }
             }
           };
@@ -105,34 +134,47 @@ export default class Socket {
     return this;
   };
 
-  map = (type: 'headers' | 'body', keys: [string], success: CalleeFunction, fail: CalleeFunction) => {
+  map = (
+    type: "headers" | "body",
+    keys: [string],
+    success: CalleeFunction,
+    fail: CalleeFunction
+  ) => {
     const types = {
-      headers: 'header',
-      body: 'body'
+      headers: "header",
+      body: "body",
     };
     const fn = (_: any, instance: any) => {
-      if (!instance['private' + type]) return { code: 400, message: 'Invalid request' };
+      if (!instance["private" + type])
+        return { code: 400, message: "Invalid request" };
       let checks = 0;
-      keys.forEach(k => {
-        if (instance['private' + type].hasOwnProperty(k)) {
+      keys.forEach((k) => {
+        if (instance["private" + type].hasOwnProperty(k)) {
           instance[types[type]] = instance[types[type]] || {};
-          instance[types[type]][k] = instance['private' + type][k];
+          instance[types[type]][k] = instance["private" + type][k];
           checks++;
         }
       });
-      if (checks === keys.length)
-        return success ? success(true) : true;
-      return fail ? fail({ code: 403, message: 'Bad Request' }) : { code: 403, message: 'Bad Request' };
+      try {
+        if (checks === keys.length) return success ? success(true) : true;
+        return fail
+          ? fail({ code: 403, message: "Bad Request" })
+          : { code: 403, message: "Bad Request" };
+      } catch (e) {
+        console.error(e);
+        return { code: 500, message: "Server Error", stack: e };
+      }
     };
     this.callee.push(fn);
     return this;
   };
 
   mapBody = (keys: [string], success: CalleeFunction, fail: CalleeFunction) => {
-    this.map('body', keys, success, fail);
+    this.map("body", keys, success, fail);
     return this;
   };
-  mapHeader = (keys: [string], success: CalleeFunction, fail: CalleeFunction) => this.map('headers', keys, success, fail);
+  mapHeader = (keys: [string], success: CalleeFunction, fail: CalleeFunction) =>
+    this.map("headers", keys, success, fail);
 
   mapDB = (...args: [any]) => {
     const fn = (_: any, instance: SocketInstance) => {
@@ -143,24 +185,28 @@ export default class Socket {
     return this;
   };
 
-  use = (type: 'db' | 'headers' | 'body' | 'store', callback: Function) => {
+  use = (type: "db" | "headers" | "body" | "store", callback: Function) => {
     const types = {
-      db: 'db',
-      headers: 'header',
-      body: 'body',
-      store: 'store'
+      db: "db",
+      headers: "header",
+      body: "body",
+      store: "store",
     };
     const fn = (_: any, instance: any) => {
-      if (callback) {
-        return callback(instance[types[type]], instance);
-      } else return true;
+      try {
+        if (callback) {
+          return callback(instance[types[type]], instance);
+        } else return true;
+      } catch (e) {
+        console.error(e);
+      }
     };
     this.callee.push(fn);
     return this;
   };
 
-  useHeader = (callback: CalleeFunction) => this.use('headers', callback);
-  useDB = (callback: CalleeFunction) => this.use('db', callback);
-  useBody = (callback: CalleeFunction) => this.use('body', callback);
-  useStore = (callback: CalleeFunction) => this.use('store', callback);
+  useHeader = (callback: CalleeFunction) => this.use("headers", callback);
+  useDB = (callback: CalleeFunction) => this.use("db", callback);
+  useBody = (callback: CalleeFunction) => this.use("body", callback);
+  useStore = (callback: CalleeFunction) => this.use("store", callback);
 }
